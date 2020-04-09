5982e682c173be3a781df3dbc876d93b27797d0c
James Johnson <jtaylorjohnson@gmail.com>
2020-04-09 21:58:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   
   def reverse_transfer
     binding.pry
-    if @status = "complete" 
+    if @status == "complete" 
       @sender.balance += @amount
       @receiver.balance -= @amount 
       @status = "reversed"
