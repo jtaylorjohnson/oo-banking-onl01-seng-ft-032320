14f8a115c3806756ef5b64933a537d1f1a219c7d
James Johnson <jtaylorjohnson@gmail.com>
2020-04-09 21:48:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
     
   def execute_transaction
-    if @sender.balance >= @amount && self.valid? then
+    if @sender.balance >= @amount && self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount 
       @status = "complete"
